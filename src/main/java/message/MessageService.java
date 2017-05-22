50d08605ec986f2cee48e6a992eae925083dbf6f
Unknown <j.vanthoor@student.fontys.nl>
2017-05-22 20:33:08 +0200

    Changed layout

--- src/main/java/message/MessageService.java
+++ src/main/java/message/MessageService.java
@@ -2,8 +2,10 @@ package message;
 
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.TextMessage;
 
 /**
  * Created by Jeroen0606 on 22-5-2017.
@@ -15,6 +17,15 @@ public class MessageService implements MessageListener {
 
     @Override
     public void onMessage(Message message) {
-        System.out.print(message.toString());
+        System.out.print("-------------------------");
+        try {
+            TextMessage txtMessage = (TextMessage) message;
+            String newMessage = txtMessage.getText();
+            String username = newMessage.split(": ")[0];
+            String kweet = newMessage.split(": ")[1];
+            System.out.print(username + " : " + kweet);
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
     }
 }
